d3102a59a1889249ca78b37d8670399620380f4c
Thomas Pedersen <STUDENT\tpede08@appl01.cs.aau.dk>
2013-04-10 13:22:29 +0200

    Added interface for libREST

---
+++ libRest/libREST.h
@@ -0,0 +1,53 @@
+// libREST.h
+
+/*  Copyright 2013 Aalborg University. All rights reserved.
+ *   
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY Aalborg University ''AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Aalborg University OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *  
+ *  The views and conclusions contained in the software and
+ *  documentation are those of the authors and should not be interpreted
+ *  as representing official policies, either expressed.
+ */
+
+#ifndef LIBREST_H
+#define LIBREST_H
+
+// Structs
+struct lr_instance;
+
+// Callbacks
+typedef ws_response *(*lr_cb)(struct ws_request *req);
+
+// Functions
+struct lr_instance *lr_create();
+void lr_destroy(struct lr_instance *);
+void lr_register_service(char *url,
+                         lr_cb on_get,
+                         lr_cb on_post,
+                         lr_cb on_put,
+                         lr_cb on_delete);
+struct ws_response *lr_lookup(struct ws_request *req, const char *url);
+
+#endif
