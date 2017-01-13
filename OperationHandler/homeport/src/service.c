3e4fb0816f675632f2e7ce873ed9a0f63e78d8ec
Thomas Pedersen <tp@cs.aau.dk>
2017-01-13 02:04:14 -0800

    Added version check for libmxml

--- OperationHandler/homeport/src/service.c
+++ OperationHandler/homeport/src/service.c
@@ -29,6 +29,7 @@
 #include "hp_macros.h"
 #include <ev.h>
 #include "utlist.h"
+#include <mxml.h>
 
 /**
  * Creates a HomePort structure
@@ -47,6 +48,17 @@ homePortNew( struct ev_loop *loop )
 {
     HomePort *homeport;
     alloc_struct(homeport);
+
+    // MXML header file bug results in 2.8 and 2.9 having the same version defines
+    if (MXML_MAJOR_VERSION == 2 && MXML_MINOR_VERSION == 8) {
+        fprintf("-------- IMPORTANT WARNING --------\n");
+        fprintf("You may have version 2.9 of mxml which is known to contain xml parsing errors with some HomePort modules, please check and upgrade (packages libmxml1 and libmxml-dev on Ubuntu)\n");
+        fprintf("-------- IMPORTANT WARNING --------\n");
+    } else if (MXML_MAJOR_VERSION == 2 && MXML_MINOR_VERSION == 9) {
+        fprintf("-------- IMPORTANT WARNING --------\n");
+        fprintf("You HAVE version 2.9 of mxml which is known to contain xml parsing errors with some HomePort modules, please upgrade (packages libmxml1 and libmxml-dev on Ubuntu)\n");
+        fprintf("-------- IMPORTANT WARNING --------\n");
+    }
 
     homeport->loop = loop;
 
